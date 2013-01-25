2c0d67acfa2a11631b2c6326d312594bd0b5fb5d
Russell Cullen <russellscullen@gmail.com>
2013-01-24 23:26:44 -0500

    Gcm register and push to all devices

--- app.py
+++ app.py
@@ -4,20 +4,32 @@ import os
 from gcm import GCM
 
 gcm = GCM('AIzaSyABIlZS0Ad_hG2CC4tjotYg2NMMZQqKI-o')
-data = {'msg': 'WOWWW'}
-reg_ids = ['APA91bHvDOBt0dhVzhHxBJ9D6OQfxNPqVpe4EGNogU5uWNWo1n9MJVMhRmdKXD73XtmbgvJ-9TeTz6g1CA28HnE55VlhGIY5jtIp4HWKCdnVC42Nvv3XfXILZMtGOtEm8d-iH5L1LOjPMZc4Gg0ej4LjKHIfsqM3ExmJita3LYhF6-RevbbJ828'] # Should save this on disk, not here. 
+#reg_ids = ['APA91bHvDOBt0dhVzhHxBJ9D6OQfxNPqVpe4EGNogU5uWNWo1n9MJVMhRmdKXD73XtmbgvJ-9TeTz6g1CA28HnE55VlhGIY5jtIp4HWKCdnVC42Nvv3XfXILZMtGOtEm8d-iH5L1LOjPMZc4Gg0ej4LjKHIfsqM3ExmJita3LYhF6-RevbbJ828'] # Should save this on disk, not here. 
+reg_ids = []
+
 
 class MainHandler(tornado.web.RequestHandler):
 	def get(self):
-		gcm.json_request(registration_ids=reg_ids, data=data)
 		self.write("Hello World")
 
 	def post(self):
-		print self.get_argument('id')
 		reg_ids.append(self.get_argument('id'))
+				
+
+class SendHandler(tornado.web.RequestHandler):
+	def get(self):
+		try:
+			msg = self.get_argument('msg')
+		except:
+			msg = "No Message"
+		if (len(reg_ids) > 0):
+			data = {'msg': msg}
+			gcm.json_request(registration_ids=reg_ids, data=data)
+		self.write("Sent: " + msg)
 
 application = tornado.web.Application([
-	(r"/", MainHandler)
+	(r"/", MainHandler),
+	(r"/send", SendHandler)
 ])
 
 if __name__ == "__main__":
