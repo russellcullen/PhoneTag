c9311bb27b4309058411bdc7ee971125e2ddcf2c
Jonathan Hsu <jreucypoda@gmail.com>
2013-01-25 20:55:03 -0500

    updated database_api

--- game.py
+++ game.py
@@ -3,3 +3,8 @@ class Game(object):
     	self.name = name
     	self.users = users
     	self.it = it
+
+    def fromDict(self, d):
+		for k,v in d.items():
+			if (hasattr(self, k)):
+				setattr(self, k, v)
