51ada7d8fe11932469360d5152b351022239d7d2
Burbles <burbinator@gmail.com>
2013-01-26 09:46:56 -0500

    fixed update thread functions

--- gcm_api.py
+++ gcm_api.py
@@ -9,8 +9,6 @@ from gcm import GCM
 
 gcm = GCM('AIzaSyABIlZS0Ad_hG2CC4tjotYg2NMMZQqKI-o')
 
-rs_id = "APA91bFg0W13FKARHpS0vLNkbf8eoKK4pAubK2xt-6W4TD9lxuWM0G8i8hSiQ5x_xY-tStuMePRH_w8HcCQVLbgJXVebq3xuM69vV_bLUcq1iCwMQoCHyHjQBwGDTV571rL9wUk99w52giCEYcb8ysufrZa0r06cAg"
-
 class UpdateThread(threading.Thread): 
     def run(self):
         t = time.time()
@@ -19,8 +17,8 @@ class UpdateThread(threading.Thread):
             if (time.time() > t + interval):
                 t = time.time()
                 sendUserInfoAll()
-                #updateScoresAll(t)
-                #checkFinishedAll(t)
+                updateScoresAll(t)
+                checkFinishedAll(t)
                 
 
 def sendIt(gameID):
@@ -28,23 +26,18 @@ def sendIt(gameID):
     db = DatabaseApi("test")
     game = db.getGameByName(gameID)
     it = game.it
-    #user = db.getUserByPhoneID(it)
     u = game.users
     reg_ids = []
     for x in xrange(len(u)):
         reg_ids.append(u[x])
     data = {"it": it, "gameID" : gameID}
-    print it
-    print len(reg_ids)
     response = gcm.json_request(registration_ids=reg_ids, data=data)
 
 def sendUserInfoAll():
     db = DatabaseApi('test')
     games = db.getAllUnfinishedGames()
-
     a = []
     users = []
-
     for x in games:
         
         l = db.getUsersByGame(x.name)
@@ -72,10 +65,3 @@ def sendUserInfo(gameID, reg_id):
         data[temp["phoneID"]] = json.dumps(temp)'''
     data = {'users' : json.dumps(a)}
     gcm.plaintext_request(registration_id=reg_id, data=data)
-
-t = UpdateThread()
-#t.start()
-    
-#sendUserInfoAll()
-#sendIt("game2")
-#sendUserInfo("game1", rs_id)
