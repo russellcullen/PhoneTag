720f1d10e4eb6a06901885145b69eae068009036
Jonathan Hsu <jreucypoda@gmail.com>
2013-01-25 19:06:04 -0500

    added database_api skeleton

---
+++ database_api.py
@@ -0,0 +1,31 @@
+## Directly accesses and performs operations on the MongoDB database ##
+
+import pymongo
+import os
+
+class DatabaseApi:
+
+	def __init__(self, db = 'something'):
+		pass
+
+	def newUser(user):
+		pass
+
+	def deleteUser(user):
+		pass
+
+	def updateUser(user, something):
+		pass
+
+	def newGame(game):
+		pass
+
+	def updateGame(game, something):
+		pass
+
+	def addUserToGame(user, game):
+		pass
+
+	def removeUserFromGame(user, game):
+		pass
+
